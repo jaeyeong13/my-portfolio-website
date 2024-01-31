bfb9ec135b90cb0d4e19c7b5b0dfb8070c49cfd5
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-31 22:41:38 +0900

    JS - Header

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Header에 페이지 아래로 스크롤 시 다크 스타일링을 적용한다.
+const header = document.querySelector(".header"); // 클래스 header인 것을 가진 요소들을 찾아 달라고 querySelector에 요청함
+// document: 문서 전체에 대한 정보를 갖고 있는 객체
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add("header--dark");
+  } else {
+    header.classList.remove("header--dark");
+  }
+});
