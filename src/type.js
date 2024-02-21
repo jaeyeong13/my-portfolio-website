9a9a26a3c9cf328e3f79e95f05e86ebc575f6368
jaeyeong13 <devlog.jae13@gmail.com>
2024-02-21 17:53:02 +0900

    [style] 타이핑 효과 에러 수정

--- src/type.js
+++ src/type.js
@@ -2,21 +2,22 @@
 
 new TypeIt(".home__title--strong", {
   loop: true,
-  speed: 50,
+  speed: 100,
 })
   .move(-11)
   .type("Amazing ")
-  .pause(1000)
+  .pause(1050)
   .move(null, { to: "END" })
   .delete()
   .type("Back-end Engineer")
-  .pause(1000)
+  .pause(1050)
   .move(-9)
   .delete(9)
   .type("Full-Stack")
-  .pause(1000)
+  .pause(1050)
   .delete(10)
   .type("Front-end")
-  .pause(1000)
+  .pause(1050)
+  .move(9)
   .delete()
   .go();
